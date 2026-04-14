9658f616b6adb1326f808c325d1972f428f5a891
Gryphon_80 <bernabecg@institutobenaventeqro.edu.mx>
2026-04-14 14:43:27 -0600

    Fix imports and enhance task management features
    Fixed import statement and added JSON import. Enhanced task management functionality by allowing task addition, deletion, and saving to a file.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
+mport time
+
+import json
 
 file_path = r"/home/gryphon_/Todo/TO_DO.txt" #file can change if working from home :] 
 
@@ -22,6 +24,31 @@ with open (file_path,"w") as to_do_file:
         if choice == 1:
             task_num +=1
             task_to_add = input ("add your task: ") #example -> Do the dishes
+            print(f"Adding {task_to_add}")
+            time.sleep(1)
+            tasks[task_num] = task_to_add
+
+        elif choice == 2:
+
+            print(tasks)
+
+            select_delition = int(input("select the number of the task you want to delete: "))
+
+            del tasks[select_delition]
+
+            print(tasks)
+
+        elif choice ==3:
+            print(tasks)
+
+        elif choice == 4:
+
+            for key , value in tasks.items():
+                to_do_file.write(f"{key} :{value}")
+                app_in_use = False
+
+
+
 
 
 
