0d94930bbc452b77637117225a54c77c109a3069
Gryphon_80 <bernabecg@institutobenaventeqro.edu.mx>
2026-04-15 10:57:17 -0600

    Update file creation logic in main.py
    Refactor file handling to allow user-defined file names.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-mport time
+import time
 
 import json
 
-file_path = r"/home/gryphon_/Todo/TO_DO.txt" #file can change if working from home :] 
+file_path = r"/home/gryphon_/Todo/" #file can change if working from home :] 
+
+file_name = input("What should the name of the file be: ")
+
+file_path+=file_name
+
+file_path+=".txt"
+time.sleep(1)
+print(f"Creating file {file_name}")
+time.sleep(1)
+print("File created")
+
+
+
+
 
 with open (file_path,"w") as to_do_file:
 
@@ -11,6 +25,11 @@ with open (file_path,"w") as to_do_file:
     
     task_num = 0
     tasks = {}
+
+    title = input("add a title for your task list: ").upper()
+
+    to_do_file.write(f"{title}\n")
+    to_do_file.write("\n")
 
     
 
@@ -45,10 +64,11 @@ with open (file_path,"w") as to_do_file:
 
             for key , value in tasks.items():
                 to_do_file.write(f"{key} :{value}")
+                to_do_file.write("\n")
                 app_in_use = False
 
-
-
+        else:
+            print("input not valid sorry")
 
 
 
