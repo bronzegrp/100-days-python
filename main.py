bba77327371ac0c57227ea6e6ab59089cdbdc6a4
Gryphon_80 <bernabecg@institutobenaventeqro.edu.mx>
2026-06-01 12:46:49 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+my_chords = {'latitude':20.70515495954519,'longitude' :-100.44581134777364}
+print(my_chords.keys())
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+print(url)
+
+iss = requests.get(url=url)
+print('\n')
+print(f'ISS INFO BELOW ->')
+print(iss.json())
+
+
+
