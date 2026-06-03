2904282a1503ab5bd8c613f566cf45fe74db331f
Gryphon_80 <bernabecg@institutobenaventeqro.edu.mx>
2026-06-03 10:57:15 -0600

    Fetch sunrise and sunset times from API
    Added functionality to fetch sunrise and sunset times using an external API.

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import requests
+import json
+from datetime import datetime
 
-my_chords = {'latitude':20.70515495954519,'longitude' :-100.44581134777364}
-print(my_chords.keys())
+#---------------------------
 
-url = 'http://api.open-notify.org/iss-now.json'
+time = datetime.now()
+my_time = time.time()
 
-print(url)
 
-iss = requests.get(url=url)
+#MY TIME AND LOC INFO ABOVE ->
+
+#---------------------------
+
+sun_api_url = 'https://api.sunrisesunset.io/json?lat=19.0414&lng=-98.2063&timezone=America/Chicago'
+
+sun_data = requests.get(url=sun_api_url)
+
+sun_data_json =sun_data.json()
+
+sunset = sun_data_json['results']['sunset']
+sunrise = sun_data_json['results']['sunrise']
+
+print(sunset)
+print(sunrise)
+print(f'my time : {my_time}')
+#api sun above
+
+
+'''
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+data = requests.get(url=api_url)
+
 print('\n')
-print(f'ISS INFO BELOW ->')
-print(iss.json())
-#learn git u skibidi
+
+json_data = data.json()
+
+list_keys = list(json_data.keys())
+print(list_keys)
+
+for key in list_keys:
+    print(json_data[key])
+    print('\n')
 
 
+'''
+
+#LEARN GIT STOP F***** COPYING AND PASTING UR OWN CODE
